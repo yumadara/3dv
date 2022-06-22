70181575170c3a0aa6bc3de5a2807f159c86e6bb
Erick <ga74kob@mytum.de>
2022-06-22 17:48:12 +0200

    mesh exporter updated

--- plenoxel_mesh_extraction/README.md
+++ plenoxel_mesh_extraction/README.md
@@ -1,3 +1,40 @@
-- put mesh_exporter inside the project directory (e.g: svox2/opt)
-- install pyvista
-- run file; the single argument is the path to npz of the pretrained model
+This script should be used with the same conda environment used for plenoxels.
+
+- Additionaly, one needs to install pyvista: 
+
+conda install -c conda-forge pyvista
+- Put mesh_exporter.py inside the opt directory (svox2/opt)
+- change line 723ff of svox2/svox2/svox2.py
+
+From:
+```
+l[:, 0] = torch.clamp_max(l[:, 0], gsz[0] - 2)
+l[:, 1] = torch.clamp_max(l[:, 1], gsz[1] - 2)
+l[:, 2] = torch.clamp_max(l[:, 2], gsz[2] - 2)
+```
+To:
+```
+l[:, 0] = torch.clamp_max(l[:, 0].float(), gsz[0] - 2)
+l[:, 1] = torch.clamp_max(l[:, 1].float(), gsz[1] - 2)
+l[:, 2] = torch.clamp_max(l[:, 2].float(), gsz[2] - 2)
+```
+- run script
+
+How to run the script:
+
+```
+python mesh_exporter.py <path to ckpt.npz file> <resoliton of the mesh>
+```
+E.g.:
+```
+python mesh_exporter.py ../checkpoints/ckpt_syn/256_to_512_fasttv/lego/ckpt.npz 0.2
+```
+
+The checkpoint for the synthetic lego can be found [here](https://drive.google.com/drive/folders/1rVqMxlQrWXklOAlQ--NdpiXrNcAwNORP?usp=sharing).
+I placed for e.g. in /svox2/checkpoints
+
+Currently I am running wit resolution 0.2, which is very low. It would be good to try with 0.5 and even 1 is possible
+
+-Output:
+
+The script creates a ckpt.obj and ckpt_colored.obj file in the same folder og the ckpt.npz file.
--- plenoxel_mesh_extraction/mesh_exporter.py
+++ plenoxel_mesh_extraction/mesh_exporter.py
@@ -1,4 +1,3 @@
-import numpy as np
 import pyvista as pv
 import svox2, mcubes, torch, numpy, argparse
 
@@ -6,13 +5,14 @@ import svox2, mcubes, torch, numpy, argparse
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('ckpt', type=str)
+    parser.add_argument('res', type=float)
     args = parser.parse_args()
     targetpath = args.ckpt[:-3] + "obj"
     print("result will be saved to:", targetpath)
     print("loading sparse grid")
     grid = svox2.SparseGrid.load(args.ckpt)
 
-    resfactor = 0.1#1  # increase/decrease to get higher or lower resolution meshes. Be very careful when increasing.
+    resfactor = args.res#1  # increase/decrease to get higher or lower resolution meshes. Be very careful when increasing.
     # 0.3 is nice for previewing the rough stuff
 
     resx = int(grid.shape[0] * resfactor)
@@ -22,8 +22,9 @@ def main():
     densitygrid = numpy.zeros((resx, resy, resz))
     print("converting densities to non-sparse numpy array")
     #not exactly fast, but not excrutiating slow either.
+    print("starting sampling")
     for x in range(resx):
-        print("Progress:%i %%" % (100 * x / resx))
+        #print("Progress:%i %%" % (100 * x / resx))
         xvals = [
                     -0.5 + x / resx] * resx  # an array with x-size size and filled with current x value, by default ranges -.5 to .49 something
         xvals = numpy.array(xvals)
@@ -39,34 +40,48 @@ def main():
             v = u.detach().numpy()  # turn tensor into numpy array.
             vt = v.T[0]  # turn array from shape (128,1) to (1,128)
             densitygrid[x, y] = vt  # feed the extracted line into our density array
+    print("end sampling")
 
-    densitygrid[densitygrid<0.05]=0
+    densitygrid[densitygrid < 0.05] = 0
+
+    def to_sample_space(points):
+        points[:, 0] /= resx
+        points[:, 1] /= resy
+        points[:, 2] /= resz
+        points -= 0.5
+        return points
+
     v, t = mcubes.marching_cubes(densitygrid, 20)  # adjust value to your scene. start with 0.
     mcubes.export_obj(v, t, targetpath)
+
     normals = estimate_normals(targetpath)
-    add_color(targetpath, v, normals, grid)
+    add_color(targetpath, v, normals, grid, to_sample_space)
 
 
 def estimate_normals(targetpath):
-
     mesh = pv.read(targetpath)
     mesh_w_no = mesh.compute_normals(cell_normals=False)
     normals = mesh_w_no['Normals']
-    #print(normals.shape)
     return normals
 
 
-def add_color(targetpath, v, normals, grid):
+def add_color(targetpath, v, normals, grid, to_space_func):
     c_fn = targetpath[:-4]+"_colored.obj"
-    # points might be in wrong location
-    vw = grid.grid2world(torch.from_numpy(v).to(torch.float32))#TODO do this tranformation to wolrd coordinates?
+    vw = torch.from_numpy(to_space_func(v)).to(torch.float32)
 
-    origins = vw + torch.from_numpy(10*normals).to(torch.float32)# a bit above
+    origins = vw + torch.from_numpy(normals).to(torch.float32)# a bit above
     # normal might have to point in oder direction
     dirs = torch.from_numpy(-normals).to(torch.float32)
-    rays = svox2.Rays(origins, dirs)
 
-    out_rgb1 = grid.volume_render(rays)
+    all_rgbs = []
+    batch_size = 5000
+    for batch_start in range(0, len(origins), batch_size):
+        batch_origins = origins[batch_start: batch_start + batch_size]
+        batch_dirs = dirs[batch_start: batch_start + batch_size]
+        batch_rays = svox2.Rays(batch_origins, batch_dirs)
+        batch_rgb = grid.volume_render(batch_rays)
+        all_rgbs.append(batch_rgb)
+    out_rgb1 = torch.cat(all_rgbs, dim=0)
 
     N_v = len(v)
     with open(c_fn, 'w') as outfile, open(targetpath, 'r', encoding='utf-8') as infile:
